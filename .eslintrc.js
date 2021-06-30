d1bbe4c246997d778b7a3b26723f72c6b0eea570
ramigs <ramigs@gmail.com>
2021-06-30 11:00:52 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'no-unused-vars': 'error',
+    indent: ['error', 2],
+  },
+}
